fd25db3ba68d4cab8a8d637a2e9e8347f89e409f
Kevin Virk <kevin@Kevins-MBP.LakerNation>
2018-08-18 20:07:48 -0700

    added source command

--- ZSHAutoInstall.sh
+++ ZSHAutoInstall.sh
@@ -16,7 +16,7 @@ fi
 
 #use case statement to choose OS
 case $os in
-    "ubuntu")  sudo apt install zsh &&  zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"&& git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;;
-    "centos")  sudo yum -y install zsh && zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"&& git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;;
+    "ubuntu")  sudo apt install zsh &&  zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"&& git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &&     echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ;;
+    "centos")  sudo yum -y install zsh && zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"&&git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &&     echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ;;
     *) echo "Make sure arguments are lowercase or that response is not recongized by this program";;
 esac
