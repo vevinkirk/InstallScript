70fb8a023b5d2a3e55ddf5069505b7c6c246d9b5
Kevin Virk <kevin@Kevins-MBP.LakerNation>
2018-08-18 19:33:07 -0700

    First ZSH install script

---
+++ ZSHAutoInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Hello, World!"
+echo "Knowledge is power"
+
+#if no command line arg given
+#set OS to unknown
+if [ -z $1 ]
+then
+    os="*** Unknown OS ***"
+elif [ -n $1 ]
+then
+#Make OS equal to first argument
+     os=$1
+
+fi
+
+#use case statement to choose OS
+case $os in
+    "ubuntu")  sudo apt install zsh &&  zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";;
+    "centos")  sudo yum -y install zsh && zsh --version && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";;
+    *) echo "Make sure arguments are lowercase or that response is not recongized by this program";;
+esac
+
+              
+
