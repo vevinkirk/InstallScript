0b29da564a47b0a10f5770e3615af9c0b353bf55
Kevin Virk <kevin@Kevins-MBP.LakerNation>
2018-08-24 00:01:40 -0700

    changed shell to zsh to fix substitution error

--- ZSHSyntaxHighlightingInstall.sh
+++ ZSHSyntaxHighlightingInstall.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
 
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git && echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-
-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 
+highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
