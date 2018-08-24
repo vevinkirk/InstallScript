4b8fa882a7d5cec4f357e4862a84d3d1578e34fc
vevinkirk <virkkevin15@gmail.com>
2018-08-24 16:31:53 -0700

    added support for spacemacs install

--- SpaceMacsInstall.sh
+++ SpaceMacsInstall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+wget https://ftp.gnu.org/pub/gnu/emacs/emacs-26.1.tar.gz && tar -zxvf emacs-26.1.tar.gz && cd emacs-26.1 && ./configure --with-mailutils && make && git clone https://github.com/sy120bnr/spacemacs ~/.emacs.d
