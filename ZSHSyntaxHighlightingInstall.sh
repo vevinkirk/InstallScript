5975c63311e047435c73511b7a280f69d26a0295
Kevin Virk <kevin@Kevins-MacBook-Pro-2.local>
2018-09-02 20:14:27 -0700

    removed ./in front of ZSH directory to chang where it was going to source

--- ZSHSyntaxHighlightingInstall.sh
+++ ZSHSyntaxHighlightingInstall.sh
@@ -2,4 +2,4 @@
 
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git && echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-
-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && source /zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
