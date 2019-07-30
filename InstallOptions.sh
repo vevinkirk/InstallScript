eaf9a80a80f09a7dc2adca9ac64774f2a5ea217f
vevinkirk <kevin@WindowsDesktop.localdomain>
2019-07-29 19:41:21 -0700

    adding awesomewm support

--- InstallOptions.sh
+++ InstallOptions.sh
@@ -3,13 +3,13 @@
 #Menu for Install Script
 echo "This program is constantly updated and will install applications for you. The list will be presented below and will fetch and install programs for you. This script comes with no warranty. Please use at your own risk."
 prompt='Please Enter Your Choice: '
-options=("ZSH Install" "ZSH syntax highlighting" "SpaceMacs" "Quit")
+options=("ZSH Install" "ZSH syntax highlighting" "SpaceMacs" "AwesomeWM" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "ZSH Install")
             echo "You Chose Choice ZSH Install"
-            osprompt='Choose your option in OS: '
+            osprompt='Choose your package manager: '
             osoptions=("apt" "yum" "pacman" "Quit")
             select osopt in "${osoptions[@]}"
             do
@@ -24,7 +24,7 @@ do
                         ;;
 		    "pacman")
 			echo "You chose pacman install"
-			echo "test"
+			./ZSHAutoInstall.sh pacman
 			;;
                     "Quit")
                         break
@@ -33,7 +33,6 @@ do
                        echo "invalid option $REPLY";;
                 esac
             done
-
             ;;
         "ZSH syntax highlighting")
             echo "You chose to install ZSH syntax highlighting"
@@ -43,6 +42,29 @@ do
             echo "You chose choice $REPLY which is $opt"
 	    chmod +x SpaceMacsInstall.sh && ./SpaceMacsInstall.sh
             ;;
+	"AwesomeWM")
+	    echo "You chose AwesomeWM install"
+	    osprompt='Choose your package manager: '
+	    osoptions=("apt" "pacman" "Quit")
+	    select osopt in "${osoptions[@]}"
+	    do
+	    	case $osopt in
+		    "apt")
+			echo "You chose apt install"
+			./AwesomeWMInstall.sh apt
+			;;
+		    "pacman")
+			echo "You chose pacman install"
+			./AwesomeWMInstall.sh pacman
+			;;
+		    "Quit")
+		    	break
+		    	;;
+		  *)
+			echo "invalid option $REPLY";;
+		esac
+	    done
+	    ;;	  	
         "Quit")
             break
             ;;
