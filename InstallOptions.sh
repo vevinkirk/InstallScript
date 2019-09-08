a947fb715667b83d7400a0d652d63165dc339bd5
vevinkirk <virkkevin15@gmail.com>
2019-09-08 08:54:57 -0700

    added vim support and moved all modules into module subdirectory

--- InstallOptions.sh
+++ InstallOptions.sh
@@ -2,8 +2,9 @@
 
 #Menu for Install Script
 echo "This program is constantly updated and will install applications for you. The list will be presented below and will fetch and install programs for you. This script comes with no warranty. Please use at your own risk."
+echo "If you are presented with a # without a prompt push enter and it will redisplay the current options you have."
 prompt='Please Enter Your Choice: '
-options=("ZSH Install" "ZSH syntax highlighting" "SpaceMacs" "AwesomeWM" "Quit")
+options=("Vim" "ZSH Install" "ZSH syntax highlighting" "SpaceMacs" "AwesomeWM" "tmux"  "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -16,15 +17,15 @@ do
                 case $osopt in
                     "apt")
                         echo "You chose apt Install"
-                        ./ZSHAutoInstall.sh apt
+                        ./modules/ZSHAutoInstall.sh apt
                         ;;
                     "yum")
                         echo "You chose yum Install"
-                        ./ZSHAutoInstall.sh yum
+                        ./modules/ZSHAutoInstall.sh yum
                         ;;
 		    "pacman")
 			echo "You chose pacman install"
-			./ZSHAutoInstall.sh pacman
+			./modules/ZSHAutoInstall.sh pacman
 			;;
                     "Quit")
                         break
@@ -36,11 +37,11 @@ do
             ;;
         "ZSH syntax highlighting")
             echo "You chose to install ZSH syntax highlighting"
-            chmod +x ZSHSyntaxHighlightingInstall.sh && ./ZSHSyntaxHighlightingInstall.sh
+            chmod +x ZSHSyntaxHighlightingInstall.sh && ./modules/ZSHSyntaxHighlightingInstall.sh
             ;;
         "SpaceMacs")
             echo "You chose choice $REPLY which is $opt"
-	    chmod +x SpaceMacsInstall.sh && ./SpaceMacsInstall.sh
+	    chmod +x SpaceMacsInstall.sh && ./modules/SpaceMacsInstall.sh
             ;;
 	"AwesomeWM")
 	    echo "You chose AwesomeWM install"
@@ -51,11 +52,11 @@ do
 	    	case $osopt in
 		    "apt")
 			echo "You chose apt install"
-			./AwesomeWMInstall.sh apt
+			./modules/AwesomeWMInstall.sh apt
 			;;
 		    "pacman")
 			echo "You chose pacman install"
-			./AwesomeWMInstall.sh pacman
+			./modules/AwesomeWMInstall.sh pacman
 			;;
 		    "Quit")
 		    	break
@@ -64,7 +65,30 @@ do
 			echo "invalid option $REPLY";;
 		esac
 	    done
-	    ;;	  	
+	    ;;
+	"Vim")
+    	    echo "You chose to install vim (good choice legend!)"
+	    osprompt='Choose your package manager: '
+	    osoptions=("apt" "pacman" "Quit")
+	    select osopt in "${osoptions[@]}"
+	    do
+		case $osopt in
+	            "apt")
+	    		echo "You chose apt install"
+    			./modules/VimInstall.sh apt
+			;;
+		    "pacman")
+    			echo "You chose pacman install"
+			./modules/VimInstall.sh pacman
+			;;
+		    "Quit")
+    			break
+			;;
+		*)
+			echo "invalid options $REPLY";;
+		esac
+	     done
+	     ;;	     
         "Quit")
             break
             ;;
